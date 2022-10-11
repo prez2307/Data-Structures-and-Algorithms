9425ab92407c587f11bb03c2e4766f6c0131cc29
prez2307 <86853355+prez2307@users.noreply.github.com>
2022-10-11 00:11:23 -0400

    Delete Labs directory

--- Labs/Lab1/Lab1/Debugging.cpp
+++
@@ -1,7 +0,0 @@
-﻿#include <iostream>
-void print_an_integer(int x) { std::cout << x; }
-int main() {
-	size_t x;
-	std::cin >> x;
-	print_an_integer(x);
-}
--- Labs/Lab8/HandWritten/diameter.h
+++
@@ -1,8 +0,0 @@
-struct BinaryTreeNode {
-  BinaryTreeNode* left;
-  BinaryTreeNode* right;
-  int value;
-  BinaryTreeNode(int n) : left{nullptr}, right{nullptr}, value{n} {}
-};
-
-int diameter(const BinaryTreeNode* tree);
