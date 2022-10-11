aa4f8d40260b1d613f7a6029c04762ace4b13ba4
prez2307 <86853355+prez2307@users.noreply.github.com>
2022-10-10 23:39:12 -0400

    Add files via upload

---
+++ project1.cpp
@@ -0,0 +1,15 @@
+#include "letterman.h"
+#include <getopt.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	std::ios_base::sync_with_stdio(false);
+	Letterman letter;
+	letter.getOptions(argc, argv);
+	letter.output();
+
+	return 0;
+}
